7c026d47b3b1b27d5cad6843420412a83f658053
lamppostvic <lamppostvic@gmail.com>
2025-10-17 19:57:03 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ export const config = {
   domainSource: "https://www.lamppostsocial.com/", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/eventcopy/[^/]+",
-          metaDataEndpoint: "https://yprlpjnmkaptxwvtfidx.supabase.co/functions/v1/event-meta?slug={{ page.slug }}"
+          pattern: "/eventpage/[^/]+",
+          metaDataEndpoint: "https://yprlpjnmkaptxwvtfidx.supabase.co/functions/v1/event-meta?slug={id}"  // Remove {{ page.slug }}, use {id}
       }
       // Add more patterns and their metadata endpoints as needed
   ]
